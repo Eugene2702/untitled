c727160dfd70f5a0912cdcdd8f58f8f5c3fd4fe1
gleb <example@gmail.com>
2023-03-12 16:25:05 +0300

    firstProject

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Component from "./Component";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
+          <Component/>
         </a>
       </header>
     </div>
---
+++ src/Component.jsx
@@ -0,0 +1,8 @@
+const Component = () => {
+    return (
+        <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aspernatur atque, aut, blanditiis dignissimos eos illo iure laboriosam natus numquam omnis porro sit.</p></div>
+    )
+
+}
+
+export default Component
